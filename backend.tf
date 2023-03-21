dc5c836f01e8dfa8879d7313ad95dbd8b2cc2258
okiemute82 <122937567+okiemute82@users.noreply.github.com>
2023-03-21 01:59:51 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-pluto-tf-state-bucket"
+    bucket = "my-aws-tf-state-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
